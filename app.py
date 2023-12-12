147d7b9c8a333510ea6a0df8e2f45957bcb07ec7
Joshua Alfred Jayapal <joshua.jayapal@gmail.com>
2023-12-12 17:21:36 -0500

    service location display

--- app.py
+++ app.py
@@ -1,5 +1,38 @@
 from flask import Flask, render_template, request, redirect, session
 from datetime import timedelta
+import pyodbc
+
+def get_pid_by_name(user_name):
+    # Assuming 'user_name' maps to a 'name' column in the 'Customer' table
+    # and 'pid' is a column in the same table
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=DESKTOP-KTBJ3S3\SQLEXPRESS;'
+        'DATABASE=SHEMS;'
+        'Trusted_Connection=yes;'
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT cust_id FROM Customer WHERE cust_fname = ?", user_name)
+    row = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return row.cust_id if row else None
+
+def get_service_locations_by_pid(pid):
+    # Assuming 'ServiceLocation' table has 'pid' as a foreign key to 'Customer'
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=DESKTOP-KTBJ3S3\SQLEXPRESS;'
+        'DATABASE=SHEMS;'
+        'Trusted_Connection=yes;'
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ServiceLocation WHERE cust_id = ?", pid)
+    locations = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return locations
+
 
 app = Flask(__name__)
 
@@ -18,7 +51,16 @@ def login():
 
 @app.route('/homepage')
 def homepage():
-     return render_template('homepage.html')
+    if 'user_name' in session:
+        # Assuming we have a function get_pid_by_name that gets the pid based on the user's name
+        pid = get_pid_by_name(session['user_name'])
+        
+        # Assuming we have a function get_service_locations_by_pid that gets service locations for a pid
+        service_locations = get_service_locations_by_pid(pid)
+        
+        return render_template('homepage.html', name=session['user_name'], service_locations=service_locations)
+    else:
+        return redirect('/login')
 
 
 @app.route('/logout')
